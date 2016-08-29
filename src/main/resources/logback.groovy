138b6d79488cd3699d3004ac65bd2cb305ad8d1d
Ryan Small <ryan.small@networkedinsights.com>
2016-08-28 20:32:17 -0500

    Provide logging.

---
+++ src/main/resources/logback.groovy
@@ -0,0 +1,16 @@
+appender('FILE', RollingFileAppender) {
+	file = '/var/log/softwhistle/finances-webservice.log'
+	append = true
+	encoder (PatternLayoutEncoder) {
+		pattern = '%date{ISO8601} - %level - %logger: %msg%n'
+	}
+	rollingPolicy (FixedWindowRollingPolicy) {
+		maxIndex = 5
+		fileNamePattern = 'finances-webservice.log-%i.gz'
+	}
+	triggeringPolicy(SizeBasedTriggeringPolicy) {
+        maxFileSize = '512MB'
+    }
+}
+logger('com.softwhistle', ALL, ['FILE'])
+root(INFO, ['FILE'])
